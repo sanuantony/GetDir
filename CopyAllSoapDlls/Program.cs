ceab7f9da9b933dc4b1d731a628d1fb3201c757e
sanuantony <sanuantony@gmail.com>
2020-06-27 21:44:59 +0530

    CopyDlls Phoenix

--- CopyAllSoapDlls/Program.cs
+++ CopyAllSoapDlls/Program.cs
@@ -9,29 +9,30 @@ namespace CopyAllSoapDlls
         {
             var dlls = new string[]
             {
-                "NextGen.Solaire.Components.dll",
-                "NextGen.Solaire.Components.Kbm.dll",
-                "NextGen.Solaire.Components.Kbm.Resources.dll",
-                "NextGen.Solaire.DataAccess.dll",
-                "NextGen.Solaire.DataAccess.Interfaces.dll",
-                "NextGen.Solaire.DataAccess.Kbm.dll",
-                "NextGen.Solaire.DataAccess.Kbm.Interfaces.dll",
-                "NextGen.Solaire.DataAccess.Kbm.Models.dll",
-                "NextGen.Solaire.Framework.dll",
-                "NextGen.Solaire.Generator.Models.dll",
-                "NextGen.Solaire.Host.DataAccess.dll",
-                "NextGen.Solaire.Host.DataAccess.Models.dll",
-                "NextGen.Solaire.Host.Interfaces.dll",
-                "NextGen.Solaire.Host.Services.dll",
-                "NextGen.Solaire.Logging.dll",
-                "NextGen.Solaire.Logging.Interfaces.dll",
-                "NextGen.Solaire.Models.dll",
-                "NextGen.Solaire.Models.Kbm.dll",
-                "NextGen.Solaire.Services.Kbm.dll",
-                "NextGen.Solaire.Services.Kbm.Functions.dll",
-                "NextGen.Solaire.Services.Kbm.Functions.Graphing.Percentile.dll",
-                "NextGen.Solaire.Services.Kbm.Functions.Interfaces.dll",
-                "NextGen.Solaire.Services.Kbm.Interfaces.dll"
+                "NextGen.Solaire.Components.dll"
+                ,"NextGen.Solaire.Components.Kbm.dll"
+                ,"NextGen.Solaire.Components.Kbm.Resources.dll"
+                ,"NextGen.Solaire.DataAccess.dll"
+                ,"NextGen.Solaire.DataAccess.Interfaces.dll"
+                ,"NextGen.Solaire.DataAccess.Kbm.dll"
+                ,"NextGen.Solaire.DataAccess.Kbm.Interfaces.dll"
+                ,"NextGen.Solaire.DataAccess.Kbm.Models.dll"
+                ,"NextGen.Solaire.Framework.dll"
+                ,"NextGen.Solaire.Generator.Models.dll"
+                ,"NextGen.Solaire.Host.DataAccess.dll"
+                ,"NextGen.Solaire.Host.DataAccess.Models.dll"
+                ,"NextGen.Solaire.Host.EHR.dll"
+                ,"NextGen.Solaire.Host.Interfaces.dll"
+                ,"NextGen.Solaire.Host.Services.dll"
+                ,"NextGen.Solaire.Logging.dll"
+                ,"NextGen.Solaire.Logging.Interfaces.dll"
+                ,"NextGen.Solaire.Models.dll"
+                ,"NextGen.Solaire.Models.Kbm.dll"
+                ,"NextGen.Solaire.Services.Kbm.dll"
+                ,"NextGen.Solaire.Services.Kbm.Functions.dll"
+                ,"NextGen.Solaire.Services.Kbm.Functions.Graphing.Percentile.dll"
+                ,"NextGen.Solaire.Services.Kbm.Functions.Interfaces.dll"
+                ,"NextGen.Solaire.Services.Kbm.Interfaces.dll"
             };
             try
             {
@@ -42,14 +43,27 @@ namespace CopyAllSoapDlls
                 //Taking Backup of existing files
                 foreach (var dll in dlls)
                     File.Copy(Path.Combine(@"C:\NEXTGEN", dll), Path.Combine(backupPath, dll), true);
+
                 File.Copy(Path.Combine(@"C:\NEXTGEN", "NextGen.Kbm.Fts_Soap.dll"), Path.Combine(backupPath, "NextGen.Kbm.Fts_Soap.dll"), true);
-                File.Copy(Path.Combine(@"C:\NEXTGEN", "NextGen.Solaire.TemplateUserControls.dll"), Path.Combine(backupPath, "NextGen.Solaire.TemplateUserControls.dll"), true);
+                File.Copy(Path.Combine(@"C:\NEXTGEN", "NextGen.Ace.Ui.dll"), Path.Combine(backupPath, "NextGen.Ace.Ui.dll"), true);
+                File.Copy(Path.Combine(@"C:\NEXTGEN", "NextGen.Ace.Cards.dll"), Path.Combine(backupPath, "NextGen.Ace.Cards.dll"), true);
+                File.Copy(Path.Combine(@"C:\NEXTGEN", "NextGen.Ace.Audit.dll"), Path.Combine(backupPath, "NextGen.Ace.Audit.dll"), true);
+                File.Copy(Path.Combine(@"C:\NEXTGEN", "NextGen.Kbm.Fts_Intake.dll"), Path.Combine(backupPath, "NextGen.Kbm.Fts_Intake.dll"), true);
+                File.Copy(Path.Combine(@"C:\NEXTGEN", "NextGen.Kbm.Well_Child_Soap.dll"), Path.Combine(backupPath, "NextGen.Kbm.Well_Child_Soap.dll"), true);
 
                 //Copying the new files
                 foreach (var dll in dlls)
-                    File.Copy(Path.Combine(@"D:\BananaCore\nextgen.solaire\bin", dll), Path.Combine(@"C:\NEXTGEN", dll), true);
-                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\src\NextGen.Kbm.FtsSoap\bin", "NextGen.Kbm.Fts_Soap.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Kbm.Fts_Soap.dll"), true);
-                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\src\NextGen.Kbm.FtsSoap\bin", "NextGen.Solaire.TemplateUserControls.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Solaire.TemplateUserControls.dll"), true);
+                    if (!dll.Equals("NextGen.Solaire.Host.EHR.dll"))
+                        File.Copy(Path.Combine(@"D:\BananaCore\nextgen.solaire\bin", dll), Path.Combine(@"C:\NEXTGEN", dll), true);
+                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.solaire\bin", "NextGen.Solaire.Host.EHRd.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Solaire.Host.EHR.dll"), true);
+
+                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\bin", "NextGen.Kbm.Fts_Soap.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Kbm.Fts_Soap.dll"), true);
+                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\bin", "NextGen.Ace.Ui.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Ace.Ui.dll"), true);
+                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\bin", "NextGen.Ace.Cards.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Ace.Cards.dll"), true);
+                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\bin", "NextGen.Ace.Audit.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Ace.Audit.dll"), true);
+                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\bin", "NextGen.Kbm.Fts_Intake.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Kbm.Fts_Intake.dll"), true);
+                File.Copy(Path.Combine(@"D:\BananaCore\nextgen.templates\bin", "NextGen.Kbm.Well_Child_Soap.dll"), Path.Combine(@"C:\NEXTGEN", "NextGen.Kbm.Well_Child_Soap.dll"), true);
+
                 Console.BackgroundColor = ConsoleColor.Green;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.Clear();
@@ -59,6 +73,10 @@ namespace CopyAllSoapDlls
             {
                 Console.BackgroundColor = ConsoleColor.DarkRed;
                 throw e;
+            }
+            if (Console.BackgroundColor == ConsoleColor.DarkRed)
+            {
+                Console.ReadKey();
             }
             System.Threading.Tasks.Task.Delay(1000);
         }
---
+++ TestAppsMode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppsMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Directory.GetFiles(@"C:\Users\santony\Desktop\All Dlls");
+
+            File.WriteAllLines(@"C:\Users\santony\Desktop\All.txt", list);
+        }
+    }
+}
